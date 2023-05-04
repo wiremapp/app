ec051d0232023b3282a8eca153a9593608d1e187
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-04 03:17:17 +0100

    :zap: update pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,7 @@
-import {
-  CellsComponent,
-  FlowChartComponent,
-  ScrollAreaComponent,
-} from "@/components";
 import { DashPage } from "@/components";
+import { useRouter } from "next/router";
 
-const initArr: Cell[] = [
-  {
-    id: "1",
-    content: "Page 1",
-    elements: [
-      {
-        id: "1",
-        content: "Header",
-        elements: [{ id: "1", content: "Baby Element 1" }],
-      },
-      { id: "2", content: "Content" },
-      { id: "3", content: "Footer" },
-    ],
-  },
-  {
-    id: "2",
-    content: "Page 2",
-    elements: [
-      { id: "5", content: "Header" },
-      {
-        id: "6",
-        content: "Footer",
-        elements: [{ id: "7", content: "Baby Element 2" }],
-      },
-    ],
-  },
-];
-
-export default function Index({}) {
-  return <DashPage />;
+export default function Home({ postsData }) {
+  const router = useRouter();
+  return <DashPage posts={postsData} router={router}/>;
 }
