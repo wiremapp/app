d4251c4bf672af5b04c9ebf63237f3fb53b4cd1d
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-04 03:20:51 +0100

    Create [...nextauth].ts

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import DiscordProvider from "next-auth/providers/discord";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import clientPromise from "@/utils/db";
+import jwt from "jsonwebtoken";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+      authorization: { params: { scope: "identify guilds" } },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async session({ session, token, user }) {
+      return {
+        ...session,
+        isNewUser: token.isNewUser,
+        token: signToken({ token }),
+      };
+    },
+    async jwt({ token, user, account, isNewUser }) {
+      if (account && user) {
+        token.access_token = account.access_token;
+        if (isNewUser) token.isNewUser = isNewUser;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt" as const,
+  },
+};
+
+const signToken = ({ token }) => {
+  return jwt.sign(token, process.env.NEXTAUTH_SECRET);
+};
+
+export default NextAuth(authOptions);
