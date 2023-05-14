4f441f4436b9756fc422f71168404a193832e9eb
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-14 04:52:04 +0100

    Create i18n.ts

---
+++ src/i18n.ts
@@ -0,0 +1,37 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import LocalStorageBackend from "i18next-localstorage-backend";
+
+const supportedLanguages = ["en"];
+
+const resources = {
+  en: {
+    translation: {
+      home_label: "Home",
+      features_label: "Features",
+      pricing_label: "Pricing",
+      about_label: "About",
+      docs_label: "Docs",
+      signIn_label: "Sign in",
+      openApp_label: "Open App",
+    },
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(LocalStorageBackend) 
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: false,
+    supportedLngs: supportedLanguages, 
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+    resources,
+  });
+
+export default i18n;
