6286f4469ba142c602c46895f5a28f5585c0f3d5
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-16 17:28:28 +0100

    Delete docs.tsx

--- src/pages/docs.tsx
+++
@@ -1,7 +0,0 @@
-import { MaintPage } from "@/components";
-import { useRouter } from "next/router";
-
-export default function Page() {
-  const router = useRouter();
-  return <MaintPage router={router}  customTitle={"Docs"}/>;
-}
