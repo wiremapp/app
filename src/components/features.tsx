ab7cc8896d5ca31464f7d134e614cbf7e95f4eeb
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-19 02:43:32 +0100

    Update features.tsx

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Component = (props: Props) => {
   return (
     <section>
-      <div className="z-1 flex h-full w-full flex-col section-container">
+      <div className="section-container">
         <div>
           <h1>FeaturesTitle</h1>
           <p>Description</p>
