436a281319031137574a5ccc8a6eb74b8559d851
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-24 02:13:38 +0100

    :zap: update pages

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,8 +1,10 @@
 import { MaintPage } from "@/components";
 import { useRouter } from "next/router";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export default function Page() {
   const router = useRouter();
-  return <MaintPage router={router} customTitle={"Settings"} />;
+  const { t } = useTranslation();
+  return <MaintPage router={router} customTitle={t("userSettings_desc")} />;
 }
