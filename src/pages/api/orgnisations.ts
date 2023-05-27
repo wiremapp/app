427172469d68c68bf9ba82640d433dc9988d9473
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-27 02:24:35 +0100

    :sparkles: add editor api routes

--- src/pages/api/orgnisations.ts
+++ src/pages/api/orgnisations.ts
@@ -1,27 +1,26 @@
-import { Subscriber } from "@/models";
+import { Orgnisation } from "@/models";
 import dbConnect from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
-
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { method, body } = req;
-  const { name, email } = body;
+  const { name } = body;
 
   switch (method) {
     case "POST":
-      if (!email) return res.status(400).json({ message: `Email is required` });
+      if (!name)
+        return res
+          .status(400)
+          .json({ message: `Orgnisation name is required` });
       dbConnect();
-      const subscriber = new Subscriber.defaultSchema({
-        email,
+      const orgnisation = new Orgnisation.defaultSchema({
         name,
       });
-  
-      await subscriber.save();
-      res.status(200).json({ message: `Subscribed successfully` });
-
+      await orgnisation.save();
+      res.status(200).json({ message: `Orgnisation created successfully` });
       break;
     default:
       res.status(405).json({ message: `Method ${method} not allowed.` });
--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,26 +1,23 @@
-import { Subscriber } from "@/models";
+import { Project } from "@/models";
 import dbConnect from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
-
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { method, body } = req;
-  const { name, email } = body;
+  const { name } = body;
 
   switch (method) {
     case "POST":
-      if (!email) return res.status(400).json({ message: `Email is required` });
       dbConnect();
-      const subscriber = new Subscriber.defaultSchema({
-        email,
-        name,
+      const project = new Project.defaultSchema({
+        name : name || "Untitled",
       });
   
-      await subscriber.save();
-      res.status(200).json({ message: `Subscribed successfully` });
+      await project.save();
+      res.status(200).json({ message: `Created project successfully` });
 
       break;
     default:
