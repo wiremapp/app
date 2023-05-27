213f9bf67e2a28402c9b81d13fc30a92564c026d
Hyporos <59734777+Hyporos@users.noreply.github.com>
2023-05-27 02:32:40 -0400

    Card Styling Changes

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -13,25 +13,25 @@ export const Component = (props: Props) => {
         <div>
         <h1 className="pb-12 text-2xl font-black text-white text-opacity-70 text-center tracking-widest">Features</h1>
           <div className="cards-container">
-            <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <div className="mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:-gray-600 dark:text-white xl:p-8">
               <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
             </div>{" "}
-            <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <div className="mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:-gray-600 dark:text-white xl:p-8">
               <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
             </div>{" "}
-            <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <div className="mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:-gray-600 dark:text-white xl:p-8">
               <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
             </div>{" "}
-            <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <div className="mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:-gray-600 dark:text-white xl:p-8">
               <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -14,10 +14,12 @@ export const Component = (props: Props) => {
       <div>
         <div className="w-full flex-col">
           <div className="w-full">
-            <h1 className="pt-4 pb-12 text-2xl font-black text-white text-opacity-70 text-center tracking-widest">Pricing</h1>
+            <h1 className="pb-12 pt-4 text-center text-2xl font-black tracking-widest text-white text-opacity-70">
+              Pricing
+            </h1>
           </div>
-          <div className="cards-container pb-24">
-            <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+          <div className="cards-container space-x-10 pb-24">
+            <div className="flex secondary max-w-lg flex-col  bg-white p-6 text-center text-gray-900 dark:bg-gray-800 dark:text-white xl:p-8">
               <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
@@ -98,12 +100,102 @@ export const Component = (props: Props) => {
               </ul>
               <a
                 href="#"
-                className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:ring-4  dark:text-white"
+                className="hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg bg-orange-600 px-5 py-2.5 text-center text-sm font-medium text-white focus:ring-4  dark:text-white"
               >
                 Get started
               </a>
             </div>
-            <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <div className="flex primary max-w-lg flex-col  bg-white px-6 py-6 text-center text-gray-900 dark:bg-gray-800 dark:text-white xl:px-8 xl:py-20">
+              <div className="flex grow flex-col">
+                <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
+                <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
+                  Best option for personal use & for your next project.
+                </p>
+                <div className="my-8 flex items-baseline justify-center">
+                  <span className="mr-2 text-5xl font-extrabold">Free</span>
+                  <span className="text-gray-500 dark:text-gray-400">
+                    /forever
+                  </span>
+                </div>
+                <ul role="list" className="mb-8 space-y-4 text-left">
+                  <li className="flex items-center space-x-3">
+                    <svg
+                      className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>Individual configuration</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                    <svg
+                      className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>No setup, or hidden fees</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                    <svg
+                      className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>
+                      Team size:{" "}
+                      <span className="font-semibold">1 developer</span>
+                    </span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                    <svg
+                      className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>
+                      Premium support:{" "}
+                      <span className="font-semibold">6 months</span>
+                    </span>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex grow flex-col">
+                <a
+                  href="#"
+                  className="hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg bg-orange-600 px-5 py-2.5 text-center text-sm font-medium text-white focus:ring-4  dark:text-white"
+                >
+                  Get started
+                </a>
+              </div>
+            </div>
+            <div className="flex secondary max-w-lg flex-col  bg-white p-6 text-center text-gray-900  dark:bg-gray-800 dark:text-white xl:p-8">
               <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
@@ -181,11 +273,10 @@ export const Component = (props: Props) => {
                     <span className="font-semibold">6 months</span>
                   </span>
                 </li>
-
               </ul>
               <a
                 href="#"
-                className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:ring-4  dark:text-white"
+                className="hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg bg-orange-600 px-5 py-2.5 text-center text-sm font-medium text-white focus:ring-4  dark:text-white"
               >
                 Get started
               </a>
