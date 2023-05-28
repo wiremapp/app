1ea18e452ba3c20946a41a756fc180582e145447
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-28 04:41:48 +0100

    Update features.tsx

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -16,30 +16,30 @@ export const Component = (props: Props) => {
         <div className="row">
           <h3>Features</h3>
           <div className="cards-container">
-            <div className="dark:-gray-600 mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:text-white xl:p-8">
+            <div className="flex flex-col p-6 text-center">
               <h3>Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
             </div>{" "}
-            <div className="dark:-gray-600 mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:text-white xl:p-8">
-              <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
+            <div className="flex flex-col p-6 text-center">
+              <h3>Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
             </div>{" "}
-            <div className="dark:-gray-600 mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:text-white xl:p-8">
-              <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
+            <div className="flex flex-col p-6 text-center">
+              <h3>Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
             </div>{" "}
-            <div className="dark:-gray-600 mx-auto flex max-w-lg flex-col p-6 text-center text-gray-900 dark:text-white xl:p-8">
-              <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
+            <div className="flex flex-col p-6 text-center">
+              <h3>Basic</h3>
               <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
                 Best option for personal use & for your next project.
               </p>
-            </div>
+            </div>{" "}
           </div>
         </div>
       </div>
