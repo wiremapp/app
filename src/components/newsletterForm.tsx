7174116915b52a098ef6a59d1b269bc13556acbb
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-31 21:43:21 +0100

    :sparkles: migrate popperjs > floating-ui/react

--- src/components/newsletterForm.tsx
+++ src/components/newsletterForm.tsx
@@ -1,77 +1,44 @@
 import { useEmailSubscription } from "@/hooks";
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import { ButtonComponent as Button } from "@/components";
-import { usePopper } from "react-popper";
+import { Tooltip, TooltipTrigger, TooltipContent } from "@/components/tooltip";
 
 const Component = () => {
   const { email, setEmail, isLoading, error, success, subscribe } =
     useEmailSubscription();
-  const [popperElement, setPopperElement] = useState(null);
-  const [arrowElement, setArrowElement] = useState(null);
-  const submitRef = useRef(null);
-  const { styles, attributes } = usePopper(submitRef.current, popperElement, {
-    placement: "top",
-    modifiers: [{ name: "arrow", options: { element: arrowElement } }],
-  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     subscribe();
   };
 
-  useEffect(() => {
-    if (success || error) {
-      const timeout = setTimeout(() => {
-        setPopperElement(null);
-        setArrowElement(null);
-      }, 3000);
-
-      return () => clearTimeout(timeout);
-    }
-  }, [success, error]);
-
   return (
-    <div className="flex">
-      {success && (
-        <div
-          ref={setPopperElement}
-          style={styles.popper}
-          {...attributes.popper}
-        >
-          <div ref={setArrowElement} style={styles.arrow} />
-          Subscription successful!
+    <Tooltip>
+      <TooltipTrigger>
+        {" "}
+        <div className="flex">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="mr-2 hidden rounded bg-white px-4 text-black text-opacity-70 opacity-70 md:block"
+            placeholder="Enter your email..."
+            disabled={isLoading}
+          />
+          <Button
+            className="hidden shrink-0 md:block"
+            href="/"
+            variant="primary"
+            aria-label={"Email Sign Up"}
+            space={"medium"}
+            onClick={handleSubmit}
+            disabled={isLoading}
+            type="submit"
+          >
+            {isLoading ? "Signing Up..." : "Sign Up"}
+          </Button>
         </div>
-      )}
-      {error && (
-        <div
-          ref={setPopperElement}
-          style={styles.popper}
-          {...attributes.popper}
-        >
-          <div ref={setArrowElement} style={styles.arrow} />
-          {error}
-        </div>
-      )}
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        className="mr-2 hidden rounded bg-white px-4 text-black text-opacity-70 opacity-70 md:block"
-        placeholder="Enter your email..."
-        disabled={isLoading}
-      />
-      <Button
-        className="hidden shrink-0 md:block"
-        href="/auth"
-        variant="primary"
-        aria-label={"Email Sign Up"}
-        space={"medium"}
-        onClick={handleSubmit}
-        disabled={isLoading}
-        type="submit"
-        ref={submitRef}
-      >
-        {isLoading ? "Signing Up..." : "Sign Up"}
-      </Button>
-    </div>
+      </TooltipTrigger>
+      <TooltipContent>{error|| !success ? error : "Subscription successful."}</TooltipContent>
+    </Tooltip>
   );
 };
 
