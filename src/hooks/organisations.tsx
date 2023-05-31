ab42adf5409a03f149d8e3645fcd9949ec23d739
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-31 19:05:42 +0100

    Update organisations.tsx

--- src/hooks/organisations.tsx
+++ src/hooks/organisations.tsx
@@ -1,31 +1,32 @@
 import { Dispatch, SetStateAction, useState } from "react";
 
-interface EmailSubscriptionState {
-  email: string;
+interface IType {
+  name?: string;
   isLoading: boolean;
   error: string | null;
   success: boolean;
-  setEmail: Dispatch<SetStateAction<string>>;
-  subscribe: () => Promise<void>;
+  setName: Dispatch<SetStateAction<string>>;
+  add: () => Promise<void>;
+  getAll: () => Promise<void>;
 }
 
-const Hook = (): EmailSubscriptionState => {
-  const [email, setEmail] = useState("");
+const Hook = (): IType => {
+  const [name, setName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState(false);
 
-  const subscribe = async () => {
+  const add = async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("/api/editor", {
+      const response = await fetch("/api/projects", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ name }),
       });
 
       if (response.ok) {
@@ -36,19 +37,46 @@ const Hook = (): EmailSubscriptionState => {
       }
     } catch (error) {
       setSuccess(false);
-      setError("Something went wrong. Please try again.");
+      setError(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const getAll = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("/api/projects", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        setSuccess(true);
+      } else {
+        setSuccess(false);
+        setError("Something went wrong. Please try again.");
+      }
+    } catch (error) {
+      setSuccess(false);
+      setError(error);
     }
 
     setIsLoading(false);
   };
 
   return {
-    email,
+    name,
     isLoading,
     error,
     success,
-    setEmail,
-    subscribe,
+    setName,
+    getAll,
+    add,
   };
 };
 
