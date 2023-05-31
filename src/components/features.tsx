a2507588cd86a30fb5abea48fae6be48a5665df0
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-31 21:54:22 +0100

    Update features.tsx

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -16,34 +16,16 @@ export const Component = (props: Props) => {
       <div>
         <div className="row">
           <h3>Features</h3>
-          {props.data.featuresData.map(feature =>{
-            return JSON.stringify(feature);
-          })}
+        
           <div className="cards-container">
-            <div className="flex flex-col p-6 text-center">
-              <h3>Basic</h3>
-              <p className="font-light text-gray-400 sm:text-lg">
-                Best option for personal use & for your next project.
-              </p>
-            </div>{" "}
-            <div className="flex flex-col p-6 text-center">
-              <h3>Basic</h3>
-              <p className="font-light text-gray-400 sm:text-lg">
-                Best option for personal use & for your next project.
-              </p>
-            </div>{" "}
-            <div className="flex flex-col p-6 text-center">
-              <h3>Basic</h3>
-              <p className="font-light text-gray-400 sm:text-lg">
-                Best option for personal use & for your next project.
-              </p>
-            </div>{" "}
-            <div className="flex flex-col p-6 text-center">
-              <h3>Basic</h3>
-              <p className="font-light text-gray-400 sm:text-lg">
-                Best option for personal use & for your next project.
-              </p>
-            </div>{" "}
+          {props.data.featuresData.map(feature =>{
+            return   <div key={feature.slug} className="flex flex-col p-6 text-center">
+            <h3>{feature.frontMatter.title}</h3>
+            <p className="font-light text-gray-400 sm:text-lg">
+              Best option for personal use & for your next project.
+            </p>
+          </div>;
+          })}
           </div>
         </div>
       </div>
