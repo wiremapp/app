5bebecf8331ec7d4305b7e2c0717c9dd5800eec0
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-01 00:50:55 +0100

    Update features.tsx

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   children?: any;
@@ -9,23 +10,26 @@ type Props = {
 };
 
 export const Component = (props: Props) => {
+  const { t } = useTranslation();
   return (
     <section
       className={props.variant == "secondary" ? "section-secondary-bg" : ""}
     >
       <div>
         <div className="row">
-          <h3>Features</h3>
+          <h3>{t("features_label")}</h3>
           <div className="cards-container">
             {props.data.featuresData.map((feature) => {
+              const featureTitle = feature.frontMatter.title;
+              const featureDesc = feature.frontMatter.description;
               return (
                 <div
                   key={feature.slug}
                   className="section-secondary-bg flex flex-col p-6 text-center"
                 >
-                  <h3>{feature.frontMatter.title}</h3>
+                  <h3>{featureTitle}</h3>
                   <p className="font-light text-gray-400 sm:text-lg">
-                    Best option for personal use & for your next project.
+                    {featureDesc}
                   </p>
                 </div>
               );
