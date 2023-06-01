5a3587c8c7758891326e73323281db8ba7a2c5a5
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-01 23:02:50 +0100

    :sparkles: add placeholder pages

--- src/pages/[static].tsx
+++ src/pages/[static].tsx
@@ -1,22 +1,18 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { getFiles, getPostBySlug } from "@/utils";
+import { getFiles, getStaticEntryBySlug } from "@/utils";
 import { EditorPage, StaticPage } from "@/components";
 import { useRouter } from "next/router";
 import React from "react";
 
 function Content({ frontMatter, markdownBody, appState }) {
   const router = useRouter();
-  return !appState ? (
-    <StaticPage source={{ frontMatter, markdownBody }} router={router} />
-  ) : (
-    <EditorPage router={router}  />
-  );
+  return <StaticPage source={{ frontMatter, markdownBody }} router={router} />;
 }
 
 export default Content;
 
 export async function getStaticProps({ params }: Params) {
-  const { frontMatter, markdownBody } = await getPostBySlug(params.static, "pages");
+  const { frontMatter, markdownBody } = await getStaticEntryBySlug(params.static, "pages");
 
   return {
     props: {
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import { DashPage, FAQPage } from "@/components";
+import { useRouter } from "next/router";
+import React from "react";
+
+export default function Page() {
+  const router = useRouter();
+  return <DashPage router={router} />;
+}
