54415249242829a0f38b0bbf8c99af1159c36ad4
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-28 06:05:44 +0100

    Update features.tsx

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -22,13 +22,16 @@ export const FeaturesComponent = (props: Props) => {
       <div>
         <div className="row">
           <h3>{t("features_label")}</h3>
-          <div className="grid-cols-1 md:grid-cols-3 lg:grid-cols-4 grid gap-4 mb-6">
-          {sortedData.map((feature) => {
+          <div className="mb-6 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+            {sortedData.map((feature) => {
               const featureTitle = feature.frontMatter.title;
               const featureDesc = feature.frontMatter.description;
               return (
-                <div key={feature.slug} className="shadow rounded p-4 section-secondary-bg">
-                  <div className="flex flex-col p-6 text-center space-y-1">
+                <div
+                  key={feature.slug}
+                  className="section-secondary-bg rounded p-4 shadow"
+                >
+                  <div className="flex flex-col space-y-1 p-6 text-center">
                     <h4>{featureTitle}</h4>
                     <p className="font-light text-gray-400 sm:text-lg">
                       {featureDesc}
@@ -37,8 +40,7 @@ export const FeaturesComponent = (props: Props) => {
                 </div>
               );
             })}
-       
-    </div>
+          </div>
         </div>
       </div>
     </section>
