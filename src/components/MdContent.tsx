a139b41db84491d6b50e85efccb26916860687b7
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-28 06:25:23 +0100

    Update MdContent.tsx

--- src/components/MdContent.tsx
+++ src/components/MdContent.tsx
@@ -8,9 +8,21 @@ export const StaticContentComponent = ({ source }) => {
   }, [setSource, source]);
 
   return (
-    <article className="lg:prose-md prose prose-slate flex flex-col border-b prose-img:rounded-sm prose-headings:text-gray-200 prose-p:text-slate-400">
-      {content}
-    </article>
+    // <article className="lg:prose-md prose prose-slate flex flex-col border-b prose-img:rounded-sm prose-headings:text-gray-200 prose-p:text-slate-400">
+
+    // </article>
+
+    <section className={"section-secondary-bg"}>
+      <div>
+        <div className="row">
+          <div className="mb-12 flex items-center justify-center">
+            <div className="lg:prose-md prose prose-slate flex flex-col border-b prose-headings:text-gray-200 prose-p:text-slate-400 prose-img:rounded-sm">
+              {content}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
