df75c07f747934e407470156b473ef63302ec0b4
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-28 06:05:51 +0100

    Update funcs.ts

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -54,6 +54,11 @@ export const handleAddElement = ({
   cb(newFlowChart);
 };
 
+export function validateEmail(email: string): boolean {
+  const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  return expression.test(email);
+}
+
 export const handleAddSubElement = ({
   arr,
   cellId,
