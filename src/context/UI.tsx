32f197e5f66f4cd4d22cea9e9f3eb4e863fa3718
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-08-01 19:25:09 +0100

    Create UI.tsx

---
+++ src/context/UI.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const UIStates = createContext<any>({});
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  const [newSpace, setNewSpace] = useState({ name: "" });
+  const [rtl, setRTL] = useState(false);
+  const [intLoad, setIntLoad] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIntLoad(false), 3000);
+  }, []);
+  
+  return (
+    <UIStates.Provider
+      value={{
+        setNewSpace,
+        newSpace,
+        rtl,
+        setRTL,
+        intLoad,
+        setIntLoad,
+      }}
+    >
+      {children}
+    </UIStates.Provider>
+  );
+}
+
+export default Provider;
