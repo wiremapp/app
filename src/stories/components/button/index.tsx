e75d48925b216eccd26ce9b06b2da34ecf684e5b
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-09-08 18:47:29 +0100

    :bug: fix secondary button styles

--- src/stories/components/button/index.tsx
+++ src/stories/components/button/index.tsx
@@ -1,36 +1,15 @@
-import React, { MutableRefObject } from "react";
+import React from "react";
 import styles from "./style.module.css";
-import Link from "next/link";
 
 export const Button = (props) => {
-  const Content = () => {
-    return (
-      <>
-        {props.children || (props.icon ? null : "Dolor Sit")}
-        {props.icon ? props.icon : ""}
-      </>
-    );
-  };
-
-  const className = `${props.className} ${styles["button"]}  ${
-    props.variant
-      ? styles[`button-${props.variant}`]
-      : styles["button-secondary"]
-  }  ${
-    props.space
-      ? props.space === "small"
-        ? "px-2"
-        : props.space === "medium"
-        ? "px-8"
-        : props.space === "wide"
-        ? "px-12"
-        : props.space
-      : "px-4"
-  }`;
-
   return (
-    <a {...props} className={className}>
-      <Content />
+    <a
+      className={` ${props.className} ${styles["button"]} ${
+        styles[`button-${props.variant !== "primary" ? "secondary" : "primary"}`]
+      } `}
+    >
+      {props.children || (props.icon ? null : "Dolor Sit")}
+      {props.icon ? props.icon : ""}
     </a>
   );
 };
