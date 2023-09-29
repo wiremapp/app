754f27f403700521ea27090c0114cb083205cab7
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-09-29 21:01:30 +0100

    Update index.tsx

--- src/stories/components/pages/dashboard/index.tsx
+++ src/stories/components/pages/dashboard/index.tsx
@@ -1,6 +1,9 @@
 import { LayoutComponent } from "@/stories/components/units/layout";
 import React from "react";
 import { LogoComponent } from "@/stories/components/units/logo";
+import { Button } from "../../button";
+import { signOut } from "next-auth/react";
+import { SignInModalButton } from "../../authButton";
 
 export const DashPage = (props) => {
   return (
@@ -35,7 +38,7 @@ export const DashPage = (props) => {
         </div>
 
         <div className="flex-grow">
-          <div className="flex h-[48px]">
+          <div className="flex h-[48px] grid-cols-[256px]">
             <div className="flex items-center p-6">
               <h2>Recent</h2>
             </div>
@@ -43,14 +46,57 @@ export const DashPage = (props) => {
             <div className="flex flex-grow items-center"></div>
 
             <div className="flex items-center">SearchInput</div>
+
+            {!props.auth.session ? (
+              <SignInModalButton {...props} />
+            ) : (
+              <div className="hidden sm:block">
+                <Button
+                  href="/"
+                  variant="secondary"
+                  aria-label={"Sign In"}
+                  space={"medium"}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signOut();
+                  }}
+                >
+                  {props.auth.session?.user.email}
+                </Button>
+              </div>
+            )}
           </div>
-          <div className="grid grid-cols-4 gap-4 p-6">
-  <div>01</div>
-  <div>09</div>
-  <div>09</div>
-  <div>09</div>
-  <div>09</div>
-</div>
+          <div className="grid flex-grow gap-4 p-6 md:grid-cols-2 lg:grid-cols-3">
+            {["Demo 1", "Demo 2", "Demo 3", "Demo 4", "Demo 5", "Demo 6"].map(
+              (item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="section-secondary-bg rounded-lg p-6 shadow"
+                  >
+                    <a href="#">
+                      <h5 className="mb-2 font-bold tracking-tight">
+                        Name: {item}
+                      </h5>
+                    </a>
+                    <p className="mb-2 font-bold tracking-tight">
+                      Instances: 1
+                    </p>
+
+                    <Button
+                      onClick={() => {
+                        props.router.push(`/s/${props.id}`);
+                      }}
+                      variant="primary"
+                    >
+                      {" "}
+                      Manage{" "}
+                    </Button>
+                  </div>
+                );
+              }
+            )}
+          </div>
         </div>
       </div>
     </LayoutComponent>
