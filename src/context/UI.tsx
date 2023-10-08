67a451e76238beae09d68e0b4a3b10140be9eb16
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-10-08 02:37:17 +0100

    :zap: update hooks and context

--- src/context/UI.tsx
+++ src/context/UI.tsx
@@ -1,3 +1,4 @@
+import { fetchProjects } from "@/utils/funcs";
 import { useSession } from "next-auth/react";
 import React, { createContext, useEffect, useState } from "react";
 
@@ -7,20 +8,49 @@ export function Provider({ children }: { children: React.ReactNode }) {
   const [newProject, setNewProject] = useState({ name: "" });
   const [rtl, setRTL] = useState(false);
   const { data: session, status } = useSession();
-  const [userProjects, setUserProjects] = useState([]);
-  const [projectModal, setProjectModal] = useState(true);
-
+  const [userProjects, setUserProjects] = useState(["Demo"]);
+  const [userProjectsLoading, setUserProjectsLoading] = useState(true);
+  const [projectModal, setProjectModal] = useState(false);
+  const [isElectron, setIsElectron] = useState(false);
   const [intLoad, setIntLoad] = useState(true);
+  const [isPWA, setIsPWA] = useState(false);
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      setIntLoad(false)
-    },500)
-  },[intLoad]);
+  useEffect(() => {
+    window.matchMedia("(display-mode: standalone)").addListener((e) => {
+      setIsPWA(e.matches);
+    });
+
+    if (window.matchMedia("(display-mode: standalone)").matches) {
+      setIsPWA(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    setIsElectron(userAgent.indexOf(" electron/") > -1);
+  }, []);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIntLoad(false);
+    }, 500);
+  }, [intLoad]);
+
+  useEffect(() => {
+    (async () => {
+      const projectsResponse = await fetchProjects();
+      console.log("Deeeeee",projectsResponse);
+      setUserProjects(projectsResponse.projects);
+    })().then(() => setUserProjectsLoading(false));
+    return () => {};
+  }, [userProjects]);
 
   return (
     <UIStates.Provider
       value={{
+        isElectron,
+        isPWA,
         setNewProject,
         newProject,
         rtl,
@@ -28,7 +58,7 @@ export function Provider({ children }: { children: React.ReactNode }) {
         userProjects,
         setUserProjects,
         auth: { session, status },
-        loading: {intLoad, setIntLoad}
+        loading: { intLoad, setIntLoad },
       }}
     >
       {children}
