9022d6004f30925f688ce495804a30362a47ebca
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-10-11 00:39:43 +0100

    Update sideBaL.tsx

--- src/stories/components/units/sideBaL.tsx
+++ src/stories/components/units/sideBaL.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import LogoComponent from "./logo";
 
 export const LeftSidebarComponent = (props: any) => {
 
   return (
     <nav aria-label="Site Navigation" className="flex h-full flex-row">
-      <div className="w-[64px] flex-grow">Sidebar</div>
+      <div className="w-[64px] flex-grow flex justify-center pt-4">
+
+      <LogoComponent text={false}/>
+
+      </div>
       <div
         style={{
           background: "linear-gradient(180deg, #030303 0%, #020202 100%)",
