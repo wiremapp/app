638a4ea7072569c375ce194dec3c01246e066581
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-10-11 00:42:09 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -39,7 +39,7 @@ function createMainWindow() {
 
   mainWindow.loadURL(
     process.env.NODE_ENV !== "production"
-      ? "http://localhost:3000/?appSt=true"
+      ? "http://localhost:3000/dashboard?app=true"
       : process.env.NEXT_PUBLIC_SITE_URL
   );
   //   drag(mainWindow, {
